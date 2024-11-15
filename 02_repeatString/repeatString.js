f4cf2a210eb2203a8424bb61a882b73ddeabd2e2
alec-shell <143364125+alec-shell@users.noreply.github.com>
2024-11-15 12:39:14 -0500

    moved if statement to one line in repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,5 @@
 const repeatString = function(string, num) {
-    if (num < 0) { 
-        return "ERROR";
-    }
+    if (num < 0) return "ERROR";
     let repeated = "";
     for (let i = 0; i < num; i++ ) {
         repeated += string;
