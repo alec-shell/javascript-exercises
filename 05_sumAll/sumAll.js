2bd4049ff3a8b94c48420e10bfb365b9daa9f206
alec-shell <143364125+alec-shell@users.noreply.github.com>
2024-11-15 16:04:31 -0500

    Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    if (first < 0 || last < 0 || !Number.isInteger(first) || !Number.isInteger(last)) {
+        return "ERROR";
+    }
+            let sum = 0;
+    if (first > last) {
+        for (let i = last; i <= first; i++) {
+            sum += i;
+        }
+    } 
+    else {
+        for (let i = first; i <= last; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
