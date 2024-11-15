891d91a5a151261847eb5c9e4a00d48792e332d8
alec-shell <143364125+alec-shell@users.noreply.github.com>
2024-11-15 15:46:20 -0500

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...target) {
+    let newList = [];
+    main: for (let i = 0; i < list.length; i++) {
+        for (item of target) {
+            if (list[i] === item) continue main;
+        }
+        newList.push(list[i]);
+    }
+    return newList;
 };
 
 // Do not edit below this line
