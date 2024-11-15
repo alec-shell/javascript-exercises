3cd2283331b7c83df97b3bdeff3815cea5480bb2
alec-shell <143364125+alec-shell@users.noreply.github.com>
2024-11-15 16:46:52 -0500

    Completed exercise in 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(F) {
+  let celsius = (F - 32) * (5/9);
+  if (celsius % 1) return Number(celsius.toFixed(1));
+  return celsius;
+ };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let fahrenheit = C * (9/5) + 32;
+  if (fahrenheit % 1) return Number(fahrenheit.toFixed(1));
+  return fahrenheit;
 };
 
 // Do not edit below this line
