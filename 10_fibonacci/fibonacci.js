c468468a49c4f99954cd28664c86b7d4812e0137
alec-shell <143364125+alec-shell@users.noreply.github.com>
2024-12-05 19:51:21 -0500

    completed exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    fib = parseInt(fib);
+    if (fib < 0) return "OOPS";
+    let current = 1;
+    let preceding = 0;
+    if (fib === 0) return preceding;
+    for (let i = 1; i < fib; i++) {
+        let next = current + preceding;
+        preceding = current;
+        current = next;
+    }
+    return current;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
