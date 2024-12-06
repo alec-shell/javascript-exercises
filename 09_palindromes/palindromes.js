799231f7af6fafc2ebebc7c464eb909879fe4bc6
alec-shell <143364125+alec-shell@users.noreply.github.com>
2024-12-05 19:23:02 -0500

    Completed exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function([...word]) {
+    const alphanu = [...'abcdefghijklmnopqrstuvwxyz1234567890'];   
+    let onlyAlpha = word.filter(letter => alphanu.includes(letter.toLowerCase())).join('').toLowerCase();
+    let reversed = onlyAlpha.split('').reverse().join('');
+    return onlyAlpha === reversed;
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = palindromes;
